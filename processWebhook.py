0dbef0e0bacf7531e15ff4fad119032fb6b01a5d
Ali Albakri <albakri.aliahmed@gmail.com>
2022-01-26 00:49:49 +0300

    make it better

--- processWebhook.py
+++ processWebhook.py
@@ -39,7 +39,7 @@ user_collection = db['Report']
 class Testing(Resource):
 
     def get(self):
-        # Note: the uidk is just a default value when searchBy identifier is not provided
+        # Note: the uik is just a default value when searchBy identifier is not provided
         # Note: some identifiers may be not unique...
         profile = user_collection.find_one({'name':'ali'})
 
