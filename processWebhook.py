dc058fc05bde27fc9cf0493a7f1123eb9de21680
Ali Albakri <albakri.aliahmed@gmail.com>
2022-01-25 21:21:45 +0300

    make it better

--- processWebhook.py
+++ processWebhook.py
@@ -24,39 +24,39 @@ def home():
     return "Hello World"
 
 
-
-api = Api(app)
-db_username = 'adveneerice'
-db_password = 'adveneerice123'
-db_name = 'adveneericeDB'
-cluster = MongoClient(
-    'mongodb+srv://adveneerice:adveneerice123@cluster0.blq7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = cluster['Hikaya']
-user_collection = db['Report']
-
-
-
-
-
-class Testing(Resource):
-
-    def get(self):
-        # Note: the uidk is just a default value when searchBy identifier is not provided
-        # Note: some identifiers may be not unique...
-        profile = user_collection.find_one({'name':'ali'})
-
-
-
-        return json.loads(json_util.dumps(profile))
+#
+# api = Api(app)
+# db_username = 'adveneerice'
+# db_password = 'adveneerice123'
+# db_name = 'adveneericeDB'
+# cluster = MongoClient(
+#     'mongodb+srv://adveneerice:adveneerice123@cluster0.blq7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+# db = cluster['Hikaya']
+# user_collection = db['Report']
 
 
 
 
 
-
-
-
-api.add_resource(Testing, '/get_random_story')
+# class Testing(Resource):
+#
+#     def get(self):
+#         # Note: the uidk is just a default value when searchBy identifier is not provided
+#         # Note: some identifiers may be not unique...
+#         profile = user_collection.find_one({'name':'ali'})
+#
+#
+#
+#         return json.loads(json_util.dumps(profile))
+#
+#
+#
+#
+#
+#
+#
+#
+# api.add_resource(Testing, '/get_random_story')
 
 
 
