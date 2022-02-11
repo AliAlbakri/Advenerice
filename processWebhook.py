787960121a6ffb1c73ba69c60f1ebaab34c9027c
Ali Albakri <albakri.aliahmed@gmail.com>
2022-02-11 22:38:51 +0300

    adding login,createUser

--- processWebhook.py
+++ processWebhook.py
@@ -38,6 +38,7 @@ cluster = MongoClient(
 
 db = cluster['adveneerice']
 user_collection = db['User']
+service_provider = db['Service_Provider']
 
 
 
@@ -64,11 +65,13 @@ class CreateUser(Resource):
             # Commented feilds may be used later
             # "uid": request.json['uid'], may be used for auth
             # "username": request.json['username'],
-            "name": request.json['name'],
+            "fname": request.json['name'],
+            "lname": request.json['name'],
             "email": request.json['email'],
             'password' : SHA256(request.json['password']),
             "bio": '',
             "sex": 'undetermined',
+            'isActive':True,
             # "profileImage": request.json['profileImage'],
             # 'birthDate': request.json['birthDate'],
             "createdAt": str(datetime.datetime.utcnow()),
@@ -82,6 +85,8 @@ class CreateUser(Resource):
 
 
 
+
+
 class Login(Resource):
 
     def put(self):
@@ -90,10 +95,9 @@ class Login(Resource):
 
         user = getUserProfile(user_collection.find_one({'email':email}))
 
-        if user:
+        if user and user['password'] == SHA256(request.json['password']):
             user_id = str(user["_id"]["$oid"])
-            if (user['password'] == SHA256(request.json['password'])):
-                 return {"status": "granted","user_id":user_id},200
+            return {"status": "granted","user_id":user_id},200
 
         else:
             return {"status":"email or password invalid, try again"},400
@@ -101,8 +105,70 @@ class Login(Resource):
 
 
 
+class Profile(Resource):
 
 
+    def get(self):
+        # note that getting a user profile  can be done by user id or email (unique params)
+        search_by = request.args['searchBy']
+        value = request.args[search_by]
+        profile = None
+        if search_by == 'email':
+         profile = getUserProfile(user_collection.find_one({search_by:value}))
+
+        if search_by == 'user_id' and ObjectId.is_valid(value) :
+            profile = getUserProfile(user_collection.find_one({"_id": ObjectId(value)}))
+
+        if profile:
+            profile.pop('password',None)
+            return profile,200
+
+        else:
+            response = {
+                "message": 'The profile does not exist',
+                "messageAr": "الحساب غير موجود"
+            }
+            return response, 400
+
+
+
+
+class CreateUserProvider(Resource):
+
+
+    def put(self):
+
+        # check if email is unique
+        if ( not IsEmailUnique(request.json['email'])):
+            response = {
+                "message": 'The email is not unique,please choose another email',
+                "messageAr": "البريد الالكتروني موجود من قبل، رجاء حاول استخدام بريد الكتروني اخر"
+            }
+            return response, 400
+
+
+        user_info_profile = {
+
+            "company_name": request.json['company_name'],
+            "email": request.json['email'],
+            'password' : SHA256(request.json['password']),
+            "bio": '',
+            "sex": 'undetermined',
+            'isActive':True,
+            # "profileImage": request.json['profileImage'],
+            # 'birthDate': request.json['birthDate'],
+            "createdAt": str(datetime.datetime.utcnow()),
+            "provided_activities": [{
+                "name":"",
+                "description":"",
+                "image":""
+            }]
+        }
+
+        uid = user_collection.insert_one(user_info_profile).inserted_id
+        print(uid)
+
+        return  {"user_id": f'{uid}'},201
 
 
 
@@ -111,6 +177,7 @@ class Login(Resource):
 
 api.add_resource(CreateUser, '/create_user')
 api.add_resource(Login, '/login')
+api.add_resource(Profile,'/get_user_profile')
 
 
 
